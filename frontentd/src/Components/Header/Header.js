19e7eca94f33cf5600f71f589e5bbaac1bd290d6
José Carlos Sánchez <a01174050@tec.mx>
2023-05-16 17:43:06 -0600

    Paginas

---
+++ frontentd/src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Tabs from '../Tabs'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <Tabs/>
+        <Tabs/>
+        <Tabs/>
+
+    </div>
+  )
+}
+
+export default Header
