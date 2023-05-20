922caee297b503a253eb064840b4f9d5707c23fb
José Carlos Sánchez <a01174050@tec.mx>
2023-05-19 18:10:44 -0600

    Añadi comentarios a algunos archivos

--- backend/controllers/controllers.js
+++ backend/controllers/controllers.js
@@ -1,7 +1,7 @@
 import client from '../database.js'
-
 client.connect()
 
+//Retorna los nombres, y URL de las imagenes de los cursos, en formato JSON
 export const getCourses = async (req, res) => {
     try {
         const courses = await client.query('SELECT nombre, img FROM courses')
@@ -12,6 +12,8 @@ export const getCourses = async (req, res) => {
     };
 }
 
+//Inserta en la base de datos un neuvo usuario con contraseña y usuario
+//(Creo que esto seria mejor para un sign in, y no tanto para un login)
 export const postUser = async (req, res) => {
     const username = req.body["username"]; //expecting a json object
     const password= req.body["password"]; //expecting a json object
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { getCourses, postUser } from "../controllers/controllers.js";
 const router = Router();
 
+//Cuando el servidor accede a estas rutas, se ejecutan los metodos HTTP (post, get, delete, update)
 router.post("/adduser", postUser);
 router.get('/cursos', getCourses)
 
