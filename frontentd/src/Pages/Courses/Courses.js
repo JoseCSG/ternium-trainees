92b325fdd5b24532c27dd7bff5f4e9acf1f8b41e
José Carlos Sánchez <a01174050@tec.mx>
2023-05-19 18:11:48 -0600

    Comentario

--- frontentd/src/Pages/Courses/Courses.js
+++ frontentd/src/Pages/Courses/Courses.js
@@ -7,13 +7,14 @@ const Courses = () => {
 
   const [courses, setCourses] = useState([])
 
+  //Recuperamos los cursos de la api de la base de datos
   const loadCourses = async () => {
     try
     {
         const response = await fetch('http://localhost:4000/cursos')
+        
+        //La parseamos a JSON y lo asignamos a la variable de cursos
         const jsonData = await response.json()
-
-        console.log(jsonData)
         setCourses(jsonData)
     }
     catch(error)
@@ -22,15 +23,19 @@ const Courses = () => {
     }
   }
 
+  //La neta no se bien que hace esto, pero es necesario ajjasj
   useEffect(() => {
     loadCourses()
   }, [])
+
   return (
     <div className='page'>
       <Header name= "Cursos" />
       <h1>Cursos a realizar</h1>
       <br></br>
       <div className='container'>
+        {/* Iteramos en el arreglo de cursos, y por cada elemento de cursos
+            renderizamos una tarjeta con su información */}
         {courses.map((course) => {
           return <Card nombre= {course.nombre} img = {course.img}/>;
         })}
