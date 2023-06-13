958d359a4484040dbf8b6a376fada1dccbed5003
jeannette <a01236226@tec.mx>
2023-06-13 14:55:00 -0600

    tab de rotaciones

--- frontend/src/Components/NavbarComp.js
+++ frontend/src/Components/NavbarComp.js
@@ -40,6 +40,7 @@ const NavbarComp = ({ isAuth, isAdmin, dispatch }) => {
               (
                 <>
                 {!isAdmin && <Nav.Link href="/courses">Cursos</Nav.Link>}
+                {!isAdmin && <Nav.Link href="/rotaciones">Rotaciones</Nav.Link>}
                 {!isAdmin && <Nav.Link href="/game">Juego</Nav.Link>}
                 {!isAdmin && <Nav.Link href="/profile">Perfil</Nav.Link>}
                 {isAdmin && <Nav.Link href="/data">Datos</Nav.Link>}
